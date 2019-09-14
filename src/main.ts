d107b2546579c3765b697a75e916a894e177d7f8
Matti Jokitulppo <melonmanchan@gmail.com>
2019-09-14 18:59:27 +0300

    Initial work on actual vim

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("this is main.ts");
+import { VimWasm } from "vim-wasm";
+
+const vim = new VimWasm({
+  canvas: document.getElementById("vim-canvas") as HTMLCanvasElement,
+  input: document.getElementById("vim-input") as HTMLInputElement,
+  workerScriptPath: "vim-wasm/vim.js"
+});
+
+vim.start();
